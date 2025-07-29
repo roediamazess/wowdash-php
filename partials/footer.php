8c5a5430f562cdaabaf4f21e6daf6211d7b9fc43
Rudianto <pms@powerpro.id>
2025-07-30 01:57:08 +0700

    Upload Template Awal

---
+++ partials/footer.php
@@ -0,0 +1,10 @@
+<footer class="d-footer">
+    <div class="row align-items-center justify-content-between">
+        <div class="col-auto">
+           <!-- <p class="mb-0">© 2025 -Ra-</p> -->
+        </div>
+        <div class="col-auto">
+            <p  class="text-primary-600">© 2025 -Ra- <span class="mb-0"></span> </p> 
+        </div>
+    </div>
+</footer>
